23d350bf5efd5cf27060808761062a45636cecf1
Márcio Fernando Maia <marciofernando.maia@yahoo.com.br>
2024-05-07 19:42:15 -0300

    Foi Adicionado mais exemplos de ponteiros

--- ponteiros2.c
+++ ponteiros2.c
@@ -1,3 +1,9 @@
+/*  Neste exemplo, a função trocarValores recebe dois ponteiros para inteiros (int *ptr1 e int *ptr2). 
+    Ela usa esses ponteiros para acessar os valores das variáveis originais e trocar seus valores entre si. 
+    No main(), você pode ver   como chamar essa função passando os endereços das variáveis a e b usando o operador &.
+*/
+
+
 #include <stdio.h>
 
 // Função para trocar os valores de duas variáveis usando ponteiros
@@ -20,3 +26,4 @@ int main() {
 
     return 0;
 }
+
