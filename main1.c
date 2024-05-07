f8df3d84345a49cd60b729e16f9d9f9254917ce5
Márcio Fernando Maia <59803321+marciomaia2020@users.noreply.github.com>
2024-05-07 07:04:21 -0300

    Adicionando mais funções

--- main1.c
+++ main1.c
@@ -3,17 +3,17 @@
 int main() {
     // Passo 1: Definindo uma variável x e um ponteiro y
     int x;
-    int *y;
+    int *y; // Ponteiro
 
     // Passo 2: Solicitar ao usuário para informar o valor 25 para x
     printf("Informe o valor para x: ");
-    scanf("%d", &x);
+    scanf("%d", &x); //&x o valor de x esta sendo usado como o endereço de memória de y
 
     // Passo 3: Fazendo o ponteiro y apontar para o endereço de x
     y = &x;
 
     // Passo 4: Alterando o valor no endereço apontado por y para 12
-    *y = 12;
+    *y = 12; // Recupera o conteudo da variável para qual o ponteiro aponta
 
     // Passo 5: Executando a operação y = y + 1
     y = y + 1;
@@ -26,3 +26,13 @@ int main() {
 
     return 0;
 }
+
+// Utilidade de ponteiros
+// Quando é preciso retornar mais de um valor em uma função
+// Manipulação de vetores, matrizes e strings
+// Armazenar referencias: listas encadeadas,árvores binárias, etc.
+// Alocão dinâmica de memória
+
+// Falou em vetores, matriz e strings estamos falando em ponteiros
+
+// UMA VARIÁVEL TEM UM ENDEREÇO E UM CONTEÚDO
---
+++ main2.c
@@ -0,0 +1,21 @@
+
+// Declarando a p como ponteiro
+int *p;
+int x;
+
+
+
+//  Para que eu saiba qual é o endereço de x uso o operador &
+//  Para eu fazer o p apontar para x devo fazer o p receber o endereço de memoria de x
+
+//Utilização do OPERADOR DE ENDEREÇO(&)
+p = &x;
+
+
+
+// Utilização do OPERADOR DE CONTEÚDO(*)
+x = *p; //Recupera o conteúdo da variável para qual o p aponta
+        // o *p está recuperando o valor de x ou seja dele mesmo
+
+
+        // Veja a figura interna
